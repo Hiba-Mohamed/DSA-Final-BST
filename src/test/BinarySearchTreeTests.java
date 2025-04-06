0da9500353b14f88b83184a8edd2b8b6ec197c4f
Hiba-Mohamed <hibamoha987@gmail.com>
2025-04-06 19:56:52 -0230

    styling

--- src/test/BinarySearchTreeTests.java
+++ src/test/BinarySearchTreeTests.java
@@ -1,6 +1,5 @@
-@SpringBootTest
+@Test
 public class BinarySearchTreeTests {
-
     private BinarySearchTree bst;
 
     @BeforeEach
@@ -17,8 +16,40 @@ public class BinarySearchTreeTests {
         bst.insert(70);
         bst.insert(60);
         bst.insert(80);
-
         List<Integer> expected = Arrays.asList(20, 30, 40, 50, 60, 70, 80);
+        assertEquals(expected, bst.inorderTraversal());
+    }
+
+    @Test
+    public void testSearchFound() {
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(15);
+        assertTrue(bst.search(5));
+        assertTrue(bst.search(10));
+        assertTrue(bst.search(15));
+    }
+
+    @Test
+    public void testSearchNotFound() {
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(15);
+        assertFalse(bst.search(20));
+        assertFalse(bst.search(0));
+    }
+
+    @Test
+    public void testInorderTraversalEmptyTree() {
+        List<Integer> expected = List.of();
+        assertEquals(expected, bst.inorderTraversal());
+    }
+
+    @Test
+    public void testInsertDuplicateValue() {
+        bst.insert(10);
+        bst.insert(10);
+        List<Integer> expected = List.of(10);
         assertEquals(expected, bst.inorderTraversal());
     }
 }
